e44f6c987c9f1f939b9d47f149adaabc50a9e8ae
Pradyuman <your-email@example.com>
2026-03-16 07:55:12 +0000

    Added Jenkin236cate for pipeline test

--- scripts/this_cert_check.sh
+++ scripts/this_cert_check.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-CERT_DIR="/home/svc_account_wso2/SIT_MI"
+CERT_DIR="/home/svc_account_wso2/SIT_MI_Docker_Project"
 
 echo "Checking for certificate files in $CERT_DIR..."
 
-CERT_FOUND=$(find "$CERT_DIR" -type f \( -name "*.crt" -o -name "*.cer" \) | head -n 1)
+CERT_FOUND=$(find "$CERT_DIR" -maxdepth 1 -type f \( -name "*.crt" -o -name "*.cer" \) | head -n 1)
 
 if [ -n "$CERT_FOUND" ]; then
     echo "Certificate file detected: $CERT_FOUND"
